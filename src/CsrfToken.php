a176a993f6b70da49ae2ab10a7f58cf88f9f2474
adelolmog <adelolmog@gmail.com>
2016-11-23 00:21:09 +0100

    Added the CsrfTokenManager

---
+++ src/CsrfToken.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DelOlmo\Token;
+
+/**
+ * A cross-site request forgery (CSRF) token object.
+ *
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class CsrfToken extends Token {
+    
+}
---
+++ src/Manager/CsrfTokenManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DelOlmo\Token\Manager;
+
+use DelOlmo\Token\Encoder\DummyTokenEncoder;
+use DelOlmo\Token\Generator\UriSafeTokenGenerator;
+use DelOlmo\Token\Storage\SessionTokenStorage;
+
+/**
+ * An object that manages cross-site request forgery (CSRF) tokens.
+ *
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class CsrfTokenManager extends TokenManager {
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $generator = new UriSafeTokenGenerator(64);
+        $encoder = new DummyTokenEncoder();
+        $storage = new SessionTokenStorage('_csrf');
+        
+        parent::__construct($generator, $encoder, $storage);
+    }
+    
+}
