c8e525b36718238c6ceecad48bcc251368581379
adelolmog <adelolmog@gmail.com>
2016-11-22 13:16:22 +0100

    Fixed the token managers

--- src/DelOlmo/Token/Manager/DeactivableTokenManager.php
+++ src/DelOlmo/Token/Manager/DeactivableTokenManager.php
@@ -4,51 +4,53 @@ namespace DelOlmo\Token\Manager;
 
 use DelOlmo\Token\Encoder\TokenEncoderInterface as Encoder;
 use DelOlmo\Token\Generator\TokenGeneratorInterface as Generator;
-use DelOlmo\Token\Storage\TokenStorageInterface as Storage;
+use DelOlmo\Token\Storage\DeactivableTokenStorageInterface as Storage;
 use DelOlmo\Token\Encoder\NativePasswordTokenEncoder;
-use DelOlmo\Token\Storage\NativeSessionTokenStorage;
+use DelOlmo\Token\Storage\NativeSessionDeactivableTokenStorage;
 use DelOlmo\Token\Generator\UriSafeTokenGenerator;
-use DelOlmo\Token\Exception\TokenNotFoundException;
 
 /**
  * @author Antonio del Olmo García <adelolmog@gmail.com>
  */
-class ExpirableTokenManager extends TokenManager implements ExpirableTokenManagerInterface
+class DeactivableTokenManager extends ExpirableTokenManager implements ExpirableTokenManagerInterface
 {
 
     /**
-     * @var string
-     */
-    const TOKEN_TIMEOUT = '+1 day';
-
-    /**
-     * @var \DelOlmo\Token\Encoder\TokenEncoderInterface
-     */
-    protected $encoder;
-
-    /**
-     * @var \DelOlmo\Token\Generator\TokenGeneratorInterface
-     */
-    protected $generator;
-
-    /**
-     * @var \DelOlmo\Token\Storage\TokenStorageInterface
+     * @var \DelOlmo\Token\Storage\ExpirableTokenStorageInterface
      */
     protected $storage;
 
     /**
+     * Constructor.
+     *
+     * @param \DelOlmo\Token\Generator\TokenGeneratorInterface $generator
+     * @param \DelOlmo\Token\Encoder\TokenEncoderInterface $encoder
+     * @param \DelOlmo\Token\Storage\ExpirableTokenStorageInterface $storage
+     * @param \DateTime $timeout
+     */
+    public function __construct(Generator $generator = null, Encoder $encoder = null, Storage $storage = null, \DateTime $timeout = null)
+    {
+        $this->encoder = $encoder ?? new NativePasswordTokenEncoder();
+        $this->generator = $generator ?? new UriSafeTokenGenerator();
+        $this->storage = $storage ?? new NativeSessionDeactivableTokenStorage();
+        $this->timeout = $timeout ?? new \DateTime(static::TOKEN_TIMEOUT);
+    }
+
+
+    /**
      * {@inheritdoc}
      */
-    public function generateToken(string $tokenId, \DateTime $expiresAt = null): string
+    public function generateToken(string $tokenId, \DateTime $expiresAt = null, bool $active = true): string
     {
-        // Value, before hashing
+        // Value, before hashing, and timeout
         $value = $this->generator->generateToken($tokenId);
+        $timeout = $expiresAt ?? new \DateTime(static::TOKEN_TIMEOUT);
 
         // Hash the value using the provided encoder
         $hash = $this->encoder->hash($value);
 
         // Store the hashed value
-        $this->storage->setToken($tokenId, $hash);
+        $this->storage->setToken($tokenId, $hash, $timeout, $active);
 
         // Return the value before hashing
         return $value;
--- src/DelOlmo/Token/Manager/ExpirableTokenManager.php
+++ src/DelOlmo/Token/Manager/ExpirableTokenManager.php
@@ -4,11 +4,10 @@ namespace DelOlmo\Token\Manager;
 
 use DelOlmo\Token\Encoder\TokenEncoderInterface as Encoder;
 use DelOlmo\Token\Generator\TokenGeneratorInterface as Generator;
-use DelOlmo\Token\Storage\TokenStorageInterface as Storage;
+use DelOlmo\Token\Storage\ExpirableTokenStorageInterface as Storage;
 use DelOlmo\Token\Encoder\NativePasswordTokenEncoder;
-use DelOlmo\Token\Storage\NativeSessionTokenStorage;
+use DelOlmo\Token\Storage\NativeSessionExpirableTokenStorage;
 use DelOlmo\Token\Generator\UriSafeTokenGenerator;
-use DelOlmo\Token\Exception\TokenNotFoundException;
 
 /**
  * @author Antonio del Olmo García <adelolmog@gmail.com>
@@ -17,38 +16,50 @@ class ExpirableTokenManager extends TokenManager implements ExpirableTokenManage
 {
 
     /**
-     * @var string
+     * @var string The default interval on which tokens expire by default
      */
     const TOKEN_TIMEOUT = '+1 day';
 
     /**
-     * @var \DelOlmo\Token\Encoder\TokenEncoderInterface
-     */
-    protected $encoder;
-
-    /**
-     * @var \DelOlmo\Token\Generator\TokenGeneratorInterface
-     */
-    protected $generator;
-
-    /**
-     * @var \DelOlmo\Token\Storage\TokenStorageInterface
+     * @var \DelOlmo\Token\Storage\ExpirableTokenStorageInterface
      */
     protected $storage;
+
+    /**
+     * @var \DateTime The date on which ExpirableToken objects expire by default
+     */
+    protected $timeout;
+
+    /**
+     * Constructor.
+     *
+     * @param \DelOlmo\Token\Generator\TokenGeneratorInterface $generator
+     * @param \DelOlmo\Token\Encoder\TokenEncoderInterface $encoder
+     * @param \DelOlmo\Token\Storage\ExpirableTokenStorageInterface $storage
+     * @param \DateTime $timeout
+     */
+    public function __construct(Generator $generator = null, Encoder $encoder = null, Storage $storage = null, \DateTime $timeout = null)
+    {
+        $this->encoder = $encoder ?? new NativePasswordTokenEncoder();
+        $this->generator = $generator ?? new UriSafeTokenGenerator();
+        $this->storage = $storage ?? new NativeSessionExpirableTokenStorage();
+        $this->timeout = $timeout ?? new \DateTime(static::TOKEN_TIMEOUT);
+    }
 
     /**
      * {@inheritdoc}
      */
     public function generateToken(string $tokenId, \DateTime $expiresAt = null): string
     {
-        // Value, before hashing
+        // Value, before hashing, and timeout
         $value = $this->generator->generateToken($tokenId);
+        $timeout = $expiresAt ?? $this->timeout;
 
         // Hash the value using the provided encoder
         $hash = $this->encoder->hash($value);
 
         // Store the hashed value
-        $this->storage->setToken($tokenId, $hash);
+        $this->storage->setToken($tokenId, $hash, $timeout);
 
         // Return the value before hashing
         return $value;
