dc5763e8b6f5de4db64f9da583040957cb3d58bd
adelolmog <adelolmog@gmail.com>
2016-11-22 13:32:42 +0100

    Native session token storage

---
+++ src/DelOlmo/Token/Storage/NativeSessionExpirableTokenStorage.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace DelOlmo\Token\Storage;
+
+use DelOlmo\Token\Exception\TokenNotFoundException;
+
+/**
+ * Token storage that uses PHP's native session handling.
+ *
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class NativeSessionExpirableTokenStorage implements ExpirableTokenStorageInterface
+{
+
+    /**
+     * The namespace used to store values in the session.
+     *
+     * @var string
+     */
+    const SESSION_NAMESPACE = '_token';
+
+    /**
+     * @var bool
+     */
+    private $sessionStarted = false;
+
+    /**
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * Initializes the storage with a session namespace.
+     *
+     * @param string $namespace The namespace under which the token is stored
+     * in the session.
+     */
+    public function __construct($namespace = self::SESSION_NAMESPACE)
+    {
+        $this->namespace = $namespace;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $store = $_SESSION[$this->namespace];
+
+        if (!isset($store[$tokenId]) ||
+                !$store[$tokenId]['expiresAt'] instanceof \DateTime ||
+                $store[$tokenId]['expiresAt'] < new \DateTime()) {
+            $str = "No valid token exists for the given id '%s'.";
+            $message = sprintf($str, $tokenId);
+            throw new TokenNotFoundException($message);
+        }
+
+        return (string) $store[$tokenId]['value'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setToken(string $tokenId, string $value, \DateTime $expiresAt)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $_SESSION[$this->namespace][$tokenId] = [
+            'value' => $value,
+            'expiresAt' => $expiresAt
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $store = $_SESSION[$this->namespace];
+
+        if (!isset($store[$tokenId]) ||
+                !$store[$tokenId]['expiresAt'] instanceof \DateTime ||
+                $store[$tokenId]['expiresAt'] < new \DateTime()) {
+            return false;
+        }
+
+        // A valid token exists if has not been expired yet
+        return $store[$tokenId]['value'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $token = isset($_SESSION[$this->namespace][$tokenId]) ?
+                (string) $_SESSION[$this->namespace][$tokenId] :
+                null;
+
+        unset($_SESSION[$this->namespace][$tokenId]);
+
+        return $token;
+    }
+
+    /**
+     * Simple helper function to start the session.
+     *
+     * @return void
+     */
+    private function startSession()
+    {
+        if (\PHP_SESSION_NONE === session_status()) {
+            session_start();
+        }
+
+        $this->sessionStarted = true;
+    }
+
+}
---
+++ src/DelOlmo/Token/Storage/NativeSessionTokenStorage.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace DelOlmo\Token\Storage;
+
+use DelOlmo\Token\Exception\TokenNotFoundException;
+
+/**
+ * Token storage that uses PHP's native session handling.
+ *
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class NativeSessionTokenStorage implements TokenStorageInterface
+{
+
+    /**
+     * The namespace used to store values in the session.
+     *
+     * @var string
+     */
+    const SESSION_NAMESPACE = '_token';
+
+    /**
+     * @var bool
+     */
+    private $sessionStarted = false;
+
+    /**
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * Initializes the storage with a session namespace.
+     *
+     * @param string $namespace The namespace under which the token is stored
+     * in the session.
+     */
+    public function __construct($namespace = self::SESSION_NAMESPACE)
+    {
+        $this->namespace = $namespace;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        if (!isset($_SESSION[$this->namespace][$tokenId])) {
+            $str = "No valid token exists for the given id '%s'.";
+            $message = sprintf($str, $tokenId);
+            throw new TokenNotFoundException($message);
+        }
+
+        return (string) $_SESSION[$this->namespace][$tokenId];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setToken(string $tokenId, string $token)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $_SESSION[$this->namespace][$tokenId] = (string) $token;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        return isset($_SESSION[$this->namespace][$tokenId]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeToken(string $tokenId)
+    {
+        if (!$this->sessionStarted) {
+            $this->startSession();
+        }
+
+        $token = isset($_SESSION[$this->namespace][$tokenId]) ?
+                (string) $_SESSION[$this->namespace][$tokenId] :
+                null;
+
+        unset($_SESSION[$this->namespace][$tokenId]);
+
+        return $token;
+    }
+
+    /**
+     * Simple helper function to start the session.
+     *
+     * @return void
+     */
+    private function startSession()
+    {
+        if (\PHP_SESSION_NONE === session_status()) {
+            session_start();
+        }
+
+        $this->sessionStarted = true;
+    }
+
+}
