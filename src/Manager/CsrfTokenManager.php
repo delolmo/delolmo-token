5a875a12e9098edab1e28f4d4a226d01a29eedc5
adelolmog <adelolmog@gmail.com>
2016-11-23 19:22:19 +0100

    Made CSRF expire after 24 hours by default

--- src/Manager/CsrfTokenManager.php
+++ src/Manager/CsrfTokenManager.php
@@ -4,15 +4,21 @@ namespace DelOlmo\Token\Manager;
 
 use DelOlmo\Token\Encoder\DummyTokenEncoder;
 use DelOlmo\Token\Generator\UriSafeTokenGenerator;
-use DelOlmo\Token\Storage\Session\SessionTokenStorage;
+use DelOlmo\Token\Storage\Session\SessionExpirableTokenStorage;
 
 /**
- * An object that manages cross-site request forgery (CSRF) tokens.
+ * An object to manager cross-site request forgery (CSRF) tokens.
  *
  * @author Antonio del Olmo García <adelolmog@gmail.com>
  */
-class CsrfTokenManager extends TokenManager {
+class CsrfTokenManager extends ExpirableTokenManager
+{
 
+    /**
+     * @var string The default interval on which tokens expire by default
+     */
+    const TOKEN_TIMEOUT = '+24 hour';
+    
     /**
      * Constructor
      */
@@ -20,9 +26,8 @@ class CsrfTokenManager extends TokenManager {
     {
         $generator = new UriSafeTokenGenerator(64);
         $encoder = new DummyTokenEncoder();
-        $storage = new SessionTokenStorage('_csrf');
+        $storage = new SessionExpirableTokenStorage('_csrf');
         
         parent::__construct($generator, $encoder, $storage);
     }
-    
 }
